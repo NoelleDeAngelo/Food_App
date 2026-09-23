c403b045ed9b36bf4823efb7bfc1ead7cbd6fae2
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2026-09-23 13:05:34 -0700

    Add category filtering to FilterableFoodList

--- src/components/CategoryChip.tsx
+++ src/components/CategoryChip.tsx
@@ -1,9 +1,9 @@
 import type { CategorySummary } from "@/types";
 import styles from "./CategoryChip.module.css";
 
-export default function CategoryChip({ category }: { category: CategorySummary }) {
+export default function CategoryChip({ category, onClick }: { category: CategorySummary; onClick: () => void }) {
   return (
-    <div className={styles.categoryChip}>
+    <div className={styles.categoryChip} onClick={onClick}>
       <span>{category.name}</span>
     </div>
   );
--- src/components/FilterableFoodList.tsx
+++ src/components/FilterableFoodList.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import type { ItemWithTagsAndCategories, CategorySummary } from "@/types";
 import CategoryChip from "./CategoryChip";
 import FoodCard from "./FoodCard";
 import styles from "./FilterableFoodList.module.css";
+import { useState } from "react";
+
 
 export default function FilterableFoodList({
   categories,
@@ -10,15 +14,33 @@ export default function FilterableFoodList({
   categories: CategorySummary[];
   items: ItemWithTagsAndCategories[];
   }) {
-  
+
+  const [selectedCategory, setSelectedCategory] = useState<CategorySummary | null>(null);
+
+  const[filteredItems, setFilteredItems] = useState<ItemWithTagsAndCategories[]>(items);
+
+
+  const handleCategoryClick = (category: CategorySummary| null) => {
+    if (category === null) {
+      setSelectedCategory(null);
+      setFilteredItems(items);
+    } else {
+      setSelectedCategory(category);
+      setFilteredItems(items.filter((item) => item.categories.some((cat) => cat.id === category.id)));
+    }
+  };
+
+
+
   return (
     <div className={styles.filterableFoodList}>
       <div className={styles.categories}>
+        <div className={styles.chip} onClick={() => handleCategoryClick(null)}><span>All</span></div>
         {categories.map((category) => (
-          <CategoryChip key={category.id} category={category} />
+          <CategoryChip key={category.id} category={category} onClick={() => handleCategoryClick(category)} />
         ))}
       </div>
-      {items.map((item) => (
+      {filteredItems.map((item) => (
         <FoodCard key={item.id} name={item.name} tags={item.tags} />
       ))}
     </div>
