e725f9dafa65a3c46d80a88a6b40efd26c3e4143
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2026-09-23 14:26:18 -0700

    Setup component testing

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "~": fileURLToPath(new URL("./", import.meta.url)),
+    },
+  },
+
+  test: {
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+  },
+});
